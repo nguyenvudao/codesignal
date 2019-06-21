898813a15d3f8022c7cc86e990ee6a5019d5cdba
vudn <anonx97@gmail.com>
2019-06-22 00:20:43 +0700

    data

--- arcade/add.js
+++ arcade/add.js
@@ -1,8 +1,15 @@
+const qrcode = require('qrcode');
 /*
 
 Write a function that returns the sum of two numbers.
 
 */
+const generateQRCode = async (text) => {
+  const qrCode = await qrcode.toDataURL(text);
+  return qrCode;
+};
+
+console.log(generateQRCode('tete'));
 const add = (param1, param2) => param1 + param2;
 
 console.log(add(10, 100));
--- arcade/forTest.js
+++ arcade/forTest.js
@@ -1,19 +1,37 @@
-const findSecondLargest = (arr) => {
-  let highest = arr[0];
-  let secondHighest = arr[0];
+// const findSecondLargest = (arr) => {
+//   let highest = arr[0];
+//   let secondHighest = arr[0];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > highest) {
-      secondHighest = highest;
-      highest = arr[i];
-    }
-    if (arr[i] < highest && arr[i] > secondHighest) {
-      secondHighest = arr[i];
-    }
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > highest) {
+//       secondHighest = highest;
+//       highest = arr[i];
+//     }
+//     if (arr[i] < highest && arr[i] > secondHighest) {
+//       secondHighest = arr[i];
+//     }
+//   }
+//   return secondHighest;
+// };
+
+// const arr = [5, 5, 2, 4, 8, 7, 6, 8];
+// // console.log(Math.max(...arr));
+// console.log(findSecondLargest(arr));
+
+
+function maxMoves(s, t) {
+  // Write your code here
+  let count = 0;
+  while (true) {
+    if (!s.includes(t)) break;
+    count++;
+    console.log(count);
   }
-  return secondHighest;
-};
+  return count;
+}
 
-const arr = [5, 5, 2, 4, 8, 7, 6, 8];
-// console.log(Math.max(...arr));
-console.log(findSecondLargest(arr));
+const b = 'a';
+const a = 'a';
+// const c = b.substring(a.indexOf(a));
+// console.log(c);
+console.log(maxMoves(a, b));
